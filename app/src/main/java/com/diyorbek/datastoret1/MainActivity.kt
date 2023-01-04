3e5cb1c747aa5273639f954be8c63be5e8e643b5
DiyorbekJDK <diyorbekdavronov07072007@gmail.com>
2023-01-04 09:25:18 +0500

    so

---
+++ app/src/main/java/com/diyorbek/datastoret1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.diyorbek.datastoret1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
